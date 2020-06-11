57d69165aa28480efe58104956cf13e41b9a5663
hmora <312296414@ciencias.unam.mx>
2020-06-11 12:51:35 -0500

    En progreso...

--- sql/MDX.sql
+++ sql/MDX.sql
@@ -39,3 +39,48 @@ SELECT
 {[RegSales].[Europe].members, [RegSales].[North America].members} ON COLUMNS,
 [Genre].[PEGI].members ON ROWS
 FROM [VG Sales]
+
+/**
+* Consulta 6
+*/
+SELECT
+[Genre].[Genre].members ON COLUMNS,
+[Publisher].[Region].members ON ROWS
+FROM [VG Sales]
+WHERE [Platform].[Platform].[PS]
+
+/**
+* Consulta 7
+*/
+SELECT
+{[Genre].[Genre].[Adventure], [Genre].[Genre].[Role-Playing]} ON COLUMNS,
+[Publisher].[Publisher Name].[Banpresto] ON ROWS
+FROM [VG Sales]
+WHERE [Platform].[Platform].[PS]
+-- Tiene un error
+
+/**
+* Consulta 8
+*/
+SELECT
+[Genre].[Genre].[Sports] ON COLUMNS,
+[Publisher].[Publisher Name].[Banpresto] ON ROWS
+FROM [VG Sales]
+WHERE [Platform].[Platform].[PS]
+-- Arroja error
+
+/**
+* Consulta 9
+*/
+SELECT
+[Genre].[Genre].[Sports] ON COLUMNS,
+[Publisher].[Publisher Name].[Nintendo] ON ROWS
+FROM [VG Sales]
+
+/**
+* Consulta 10
+*/
+SELECT
+{[Genre].[Genre].[Sports], [Genre].[Genre].[Adventure]} ON COLUMNS,
+{[Publisher].[Publisher Name].[Pony Canyon], [Publisher].[Publisher Name].[Sunsoft]} ON ROWS
+FROM [VG Sales]
