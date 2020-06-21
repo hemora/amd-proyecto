f75836fce78c6d184711d5b2ce3144ef46cb9772
hmora <312296414@ciencias.unam.mx>
2020-06-21 15:43:58 -0500

    MDX actualizado con ejemplos para poner en los resultados

--- sql/MDX.sql
+++ sql/MDX.sql
@@ -1,92 +1,27 @@
 /**
-* Consulta 1
+* Repositorio de consultas MDX para poner en Resultados
 */
-SELECT
-[Publisher].members ON COLUMNS,
-[Genre].members ON ROWS
-FROM [VG Sales]
 
-/**
-* Consulta 2
-*/
+-- 1
 SELECT
-{[Publisher].[Publisher Name].members, [Publisher].[Region].members} ON COLUMNS,
-[Genre].[PEGI].members ON ROWS
+	[Genre].[Genre].members ON COLUMNS,
+	[Measures].[NAUnit]ON ROWS
 FROM [VG Sales]
+--
 
-/**
-* Consulta 3
-*/
+-- 2
 SELECT
-[Publisher].[Publisher Name].[THQ] ON COLUMNS,
-[Genre].[PEGI].[7] ON ROWS
+	[Games].[GameName].[Super Mario Bros. 3] ON COLUMNS,
+	{[Measures].[NAUnit], [Measures].[JPUnit], [Measures].[GlobalUnit]} ON ROWS
 FROM [VG Sales]
+--
 
-/**
-* Consulta 4
-*/
+-- 3
 SELECT
-{[Genre].[Genre].members, [Publisher].[Publisher Name].[Nintendo]} ON COLUMNS,
-[Platform].[Generation].[1] ON ROWS
+	CROSSJOIN(
+		[Publisher].[Region].members,
+		[Platform].[Platform].[GB]
+	) ON COLUMNS,
+	{[Measures].[NAUnit], [Measures].[JPUnit], [Measures].[GlobalUnit]} ON ROWS
 FROM [VG Sales]
-WHERE {[RegSales].[North America].members, [RegSales].[Japan].members}
--- Esta Consulta no arroja valores importantes
-
-/**
-* Consulta 5
-*/
-SELECT
-{[RegSales].[Europe].members, [RegSales].[North America].members} ON COLUMNS,
-[Genre].[PEGI].members ON ROWS
-FROM [VG Sales]
-
-/**
-* Consulta 6
-*/
-SELECT
-[Genre].[Genre].members ON COLUMNS,
-[Publisher].[Region].members ON ROWS
-FROM [VG Sales]
-WHERE [Platform].[Platform].[PS]
-
-/**
-* Consulta 7
-*/
-SELECT
-{[Genre].[Genre].[Adventure], [Genre].[Genre].[Role-Playing]} ON COLUMNS,
-[Publisher].[Publisher Name].[Banpresto] ON ROWS
-FROM [VG Sales]
-
-/**
-* Consulta 8
-*/
-SELECT
-[Platform].[Platform].[NES] ON COLUMNS,
-[Publisher].[Region].[Asia] ON ROWS
-FROM [VG Sales]
-
-/**
-* Consulta 9
-*/
-SELECT
-[Genre].[Genre].[Sports] ON COLUMNS,
-[Publisher].[Publisher Name].[Nintendo] ON ROWS
-FROM [VG Sales]
-
-/**
-* Consulta 10
-*/
-SELECT
-{[Genre].[Genre].[Sports], [Genre].[Genre].[Adventure]} ON COLUMNS,
-{[Publisher].[Publisher Name].[Pony Canyon], [Publisher].[Publisher Name].[Sunsoft]} ON ROWS
-FROM [VG Sales]
-
-/**
-* Consultas que arrojan error
-*/
-SELECT
-{[Genre].[Genre].[Adventure], [Genre].[Genre].[Role-Playing]} ON COLUMNS,
-[Publisher].[Publisher Name].[Banpresto] ON ROWS
-FROM [VG Sales]
-WHERE [Platform].[Platform].[PS]
 --
